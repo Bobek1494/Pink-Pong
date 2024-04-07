901c62fcc839046151cd31c7f675bf3d1e1968ee
Bobek1494 <166284288+Bobek1494@users.noreply.github.com>
2024-04-07 12:03:52 +0300

    Create code.py
    The code of Pink Pong game

---
+++ code.py
@@ -0,0 +1,47 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+  def __init__(self, player_image, player_x, player_x, player_speed, width, height):
+    super().__init__()
+    self.image = transform.scale(image.load(player_image), (width, height))
+    self.speed = player_speed
+    self.rect = self.image.get_rect()
+    self.rect.x = player_x
+    self.rect.y = player_y
+
+  def reset(self):
+    window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+  def update_r(self):
+    keys = key.get_pressed()
+    if keys[K_UP] and self.rect.y > 5:
+      self.rect.y -= self.speed
+    if keys[K_DOWN] and self.rect.y < win_height - 80:
+      self.rect.y += self.speed
+  def update_l(self):
+    keys = key.get_pressed()
+    if keys[K_W] and self.rect.y > 5:
+      self.rect.y -= self.speed
+    if  keys[K_S] and self.rect.y < win_height - 80:
+      self.rect += self.speed
+back = (200, 255, 255)
+win_width = 600
+win height = 500
+window = display.set_mode((win_width, win height))
+window.fill(back)
+
+game = True
+
+finish = False
+clock time.Clock()
+FPS = 60
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis_ball.png', 200, 200, 4, 50, 150)
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+speed_x = 3
+speed_y = 3
+while game:
